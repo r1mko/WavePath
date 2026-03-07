17375a0e578e2a24ebb63c510e8d8ab3fc24da30
r1mko <r1mko@yahoo.com>
2026-03-07 15:34:33 +0400

    гибкость триггеров

--- Assets/Scripts/Scenario/StepTrigger.cs
+++ Assets/Scripts/Scenario/StepTrigger.cs
@@ -3,20 +3,28 @@
 public class StepTrigger : MonoBehaviour
 {
     public int CurrentStep;
+    public int CollisionToUse;
+
     private bool wasUsed;
+    private int collisionCount;
 
     private void Start()
     {
         wasUsed = false;
+        collisionCount = 0;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !wasUsed)
         {
-            ActionBus.AdvanceTo(CurrentStep);
-            wasUsed = true;
-            Debug.Log($"[StepTrigger]Старт сценария с шагом: {CurrentStep}");
+            collisionCount++;
+            if (CollisionToUse <= collisionCount)
+            {
+                ActionBus.AdvanceTo(CurrentStep);
+                wasUsed = true;
+                Debug.Log($"[StepTrigger]Старт сценария с шагом: {CurrentStep}");
+            }
         }
     }
 }
